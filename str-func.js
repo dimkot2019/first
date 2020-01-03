b3d79f50f38dc90e195a4f1cf935113c99046cb5
vigdorov <vigdorov@yandex-team.ru>
2020-01-03 20:32:26 +0300

    Добавил решение по задачам строки - 15, массивы - 1-16

--- str-func.js
+++ str-func.js
@@ -11,6 +11,23 @@ const isLetter = symbol => {
     return letters.includes(symbol);
 };
 
+const wordShift = (word, shift) => {
+    let result = '';
+
+    for (let i = 0; i < word.length; i += 1) {
+        let index = i + shift;
+        while (index < 0) {
+            index += word.length;
+        }
+        while (index >= word.length) {
+            index -= word.length;
+        }
+        result += word[index];
+    }
+
+    return result;
+};
+
 // 1. Дана строка - str. Нужно вернуть количество символов в строке (учитываются любые символы, и пробелы тоже)
 // Пример строки: 'Это строка с текстом'
 
@@ -286,6 +303,27 @@ const getLongestWordAndCount = text => {
     return `${longestWord} - ${maxCount}`;
 };
 
+// 15. Дан текст. Написать функцию которая считает все символы и выводит информацию в виде такого объекта:
+// { a: 24, b: 12, !: 4, h: 90, 8: 23 }
+
+const countLetters = text => {
+    const result = {};
+
+    for (let i = 0; i < text.length; i += 1) {
+        const symbol = text[i].toLowerCase();
+
+        if (symbol !== ' ') {
+            if (result[symbol]) {
+                result[symbol] += 1;
+            } else {
+                result[symbol] = 1;
+            }
+        }
+    }
+
+    return result;
+};
+
 // 13. Тебе в руки попал зашифрованный текст означающий большую истинность для многих программистов.
 // Расшифруй его:
 /*
@@ -303,31 +341,16 @@ bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip
 */
 
 const decodeChiper = () => {
-    const encodeString = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf' +
-        'ibou fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/' +
-        'bstfTq jt uufscf uibo otf/ef uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf' +
-        'hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc uz/qvsj Fsspst tipvme' +
-        'wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj' +
-        'gvtfsf fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom' +
-        'pof pvt..pcwj xbz pu pe ju/ Bmuipvhi uibu bzx bzn puo cf wjpvtpc bu jstug' +
-        'ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt fopgu cfuufs' +
-        'boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb' +
-        'Jg fui foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO' +
-        'bsf pof ipoljoh sfbuh efbj .. fu(tm pe psfn gp tf"uip';
-
-    const obj = {};
-
-    for (let i = 0; i < encodeString.length; i += 1) {
-        const symbol = encodeString[i].toLowerCase();
-
-        if (symbol !== ' ') {
-            if (obj[symbol]) {
-                obj[symbol] += 1;
-            } else {
-                obj[symbol] = 1;
-            }
-        }
-    }
+    const encodeString = 'vujgvmCfb tj ufscfu ouib z/vhm jdjuFyqm jt fscfuu uibo jdju/jnqm fTjnqm tj scfuuf ibou ' +
+        'fy/dpnqm yDpnqmf jt cfuufs boui dbufe/dpnqmj uGmb tj fuufsc ouib oftufe/ bstfTq jt uufscf uibo otf/ef ' +
+        'uzSfbebcjmj vout/dp djbmTqf dbtft (ubsfo djbmtqf hifopv up csfbl ifu t/svmf ipvhiBmu zqsbdujdbmju fbutc ' +
+        'uz/qvsj Fsspst tipvme wfsof qbtt foumz/tjm omfttV mjdjumzfyq odfe/tjmf Jo fui dfgb pg hvjuz-bncj gvtfsf ' +
+        'fui ubujpoufnq up ftt/hv Uifsf vmetip fc pof.. boe sbcmzqsfgf zpom pof pvt..pcwj xbz pu pe ju/ Bmuipvhi ' +
+        'uibu bzx bzn puo cf wjpvtpc bu jstug ttvomf sfzpv( i/Evud xOp tj scfuuf ibou /ofwfs uipvhiBm fsofw jt ' +
+        'fopgu cfuufs boui iu++sjh x/op gJ ifu nfoubujpojnqmf tj eibs pu mbjo-fyq tju( b bec /jefb Jg fui ' +
+        'foubujpojnqmfn jt fbtz up bjo-fyqm ju znb cf b hppe jefb/ bnftqbdftO bsf pof ipoljoh sfbuh efbj .. ' +
+        'fu(tm pe psfn gp tf"uip';
+    const obj = countLetters(encodeString);
 
     const letterList = Object.keys(obj).map(key => ({letter: key, count: obj[key]}));
     const allLetterCount = letterList.reduce((prev, next) => {
@@ -340,6 +363,12 @@ const decodeChiper = () => {
         percent: Math.trunc(count / allLetterCount * 100),
     }));
 
+    const maxPercent = letterListWithPercent.reduce((prev, next) => {
+        return prev.percent > next.percent ? prev : next;
+    });
+
+    const alphabetShift = 'e'.charCodeAt(0) - maxPercent.letter.charCodeAt(0);
+
     let res = '';
 
     for (let i = 0; i < encodeString.length; i += 1) {
@@ -347,11 +376,26 @@ const decodeChiper = () => {
         if (letter === ' ') {
             res += letter;
         } else {
-            res += String.fromCharCode(encodeString.charCodeAt(i) - 1);
+            res += String.fromCharCode(encodeString.charCodeAt(i) + alphabetShift);
         }
     }
 
-    console.log(res);
+    let shift = -3;
+    let stepShift = -1;
+    let wordStepShift = 5;
+
+    let count = 0;
+
+    const wordsList = res.split(' ').map((word, i) => {
+        if (count === wordStepShift) {
+            count = 0;
+            shift += stepShift;
+        }
+        count += 1;
+        return wordShift(word, shift);
+    });
+
+    return wordsList.join(' ');
 };
 
 // 14. Реализуй шифрование и дешифрование "сендвич". Суть в следующем. Текст делиться попалам на две равные
@@ -384,4 +428,211 @@ const decodeSandwich = text => {
     }
 
     return firstHalf + secondHalf;
+};
+
+// Массивы
+// 1. Дан массив чисел. Найти сумму всех его элементов.
+
+const arrayElementsSummary = array => {
+    return array.reduce((prev, next) => prev + next);
+};
+
+// 2. Дан массив чисел. Найти среднее арифметическое четных чисел массива.
+
+const arithmeticMean = array => {
+    const filtered = array.filter(number => number % 2 === 0);
+    return arrayElementsSummary(filtered) / filtered.length;
+};
+
+// 3. Дан массив чисел. Найти число которое встречается чаще всего
+
+const repeatedNumber = array => {
+    const obj = {};
+    array.forEach(number => {
+        if (obj[number]) {
+            obj[number] += 1;
+        } else {
+            obj[number] = 1;
+        }
+    });
+
+    let maxNumber = {number: '', count: 0};
+
+    Object.keys(obj).forEach(number => {
+        const count = obj[number];
+
+        if (count > maxNumber.count) {
+            maxNumber = {number, count};
+        }
+    });
+
+    return maxNumber;
+};
+
+// 4. Дан массив чисел. Оставить только четные числа, которые деляться на 3 без остатка.
+
+const filterTwoThree = array => array.filter(number => number % 2 === 0 && number % 3 === 0);
+
+// 5. Дан массив данных. Переставить эелементы массива в обратном порядке.
+
+const reverseArray = array => {
+    let reverseArray = [];
+
+    for (let i = array.length - 1; i >= 0; i -= 1) {
+        reverseArray.push(array[i]);
+    }
+
+    return reverseArray;
+};
+
+// 6. Дано число N. Заполнить массив N-количеством случайных целых чисел от 0 до N. При этом числа не должны повторяться.
+
+const randomArray = n => {
+    const array = [];
+
+    for (let i = 0; i < n; i += 1) {
+        let randomNumber = Math.trunc(Math.random() * n);
+
+        while (array.includes(randomNumber)) {
+            randomNumber = Math.trunc(Math.random() * n);
+        }
+
+        array.push(randomNumber);
+    }
+
+    return array;
+};
+
+// 7. Дано число N - шаг геометрической прогрессии и X - число с которого начинается прогрессия,
+// Y - длина прогрессии. Вернуть массив заполненный такой прогрессией.
+
+const arithmeticProgressive = (step, start, length) => {
+    const array = [];
+
+    for (let i = 0; i < length; i += 1) {
+        array.push(start + step * i);
+    }
+
+    return array;
+};
+
+// 8. Дан массив чисел. Найти количество четных чисел в массиве.
+
+const countEvenNumbers = array => array.filter(n => n % 2 === 0).length;
+
+// 9. Дан массив всевозможных данных. Очистить его от falsebel элементов, тоесть false, 0, null и тд.
+
+const clearArray = array => array.filter(n => !!n);
+
+// 10. Дан массив чисел. Найти самое большое четное число в массиве.
+
+const maxEvenNumber = array => {
+    const evenList = array.filter(n => n % 2 === 0);
+    return Math.max(...evenList);
+};
+
+// 11. Дан массив чисел. Найти два соседних элемента, чья сумма будет наименьшей.
+
+const minAmountNeigbors = array => {
+    let minSum = {numbers: '', summary: Infinity};
+
+    for (let i = 0; i < array.length - 1; i += 1) {
+        const first = array[i];
+        const second = array[i + 1];
+
+        if (first + second < minSum.summary) {
+            minSum = {number: `${first} + ${second}`, summary: first + second};
+        }
+    }
+
+    return minSum;
+};
+
+// 12. Дан массив чисел. Найти все повторяющиеся числа и какое количество раз.
+
+const countRepeatedNumbers = array => {
+    const obj = {};
+
+    array.forEach(number => {
+        if (obj[number]) {
+            obj[number] += 1;
+        } else {
+            obj[number] = 1;
+        }
+    });
+
+    return Object.keys(obj).filter(number => {
+        const count = obj[number];
+
+        return count > 1;
+    });
+};
+
+// 13. Дан массив чисел. Удалить все элементы из массива, которые повторяются больше двух раз.
+
+const clearTripleRepeat = array => {
+    return array.filter(n => array.filter(k => k === n).length < 3);
+};
+
+// 14. Даны два массива чисел. Сформировать третий массив, который наполнен элементами которые есть и в первом и во втором массивах.
+
+const crossingArrays = (first, second) => {
+    return first.filter(n => second.includes(n));
+};
+
+// 15. Дан массив чисел. Удалить из массива все числа палиндромы.
+
+const isPalindrome = number => {
+    const stringNumber = String(number);
+
+    for (let start = 0; start < stringNumber.length / 2; start += 1) {
+        const end = stringNumber.length - 1 - start;
+        if (stringNumber[start] !== stringNumber[end]) {
+            return false;
+        }
+    }
+
+    return true;
+};
+
+const clearPalindrome = array => {
+    return array.filter(number => !isPalindrome(number));
+};
+
+// 16. Дан массив чисел. Найти наибольшую возрастающую последовательность чисел
+// в этом массиве которые идут подряд. Пример: 1, 4, 6, 10
+
+const searchSequence = array => {
+    array.push(-Infinity);
+    let maxSequence = [];
+    let sequence = [];
+
+    for (let i = 0; i < array.length; i += 1) {
+        const current = array[i];
+
+        if (!sequence.length) {
+            sequence.push(current);
+        } else {
+            const lastNumber = sequence[sequence.length - 1];
+
+            if (current >= lastNumber) {
+                sequence.push(current);
+            } else {
+                if (sequence.length > maxSequence.length) {
+                    maxSequence = sequence;
+                }
+                sequence = [current];
+            }
+        }
+    }
+
+    return maxSequence;
+};
+
+// 17. Дан массив чисел. Удалить из него числа так, чтобы оставшиеся образовали
+// наибольшую возрастающую последовательность. Для решения задачи используются методы
+// динамического программирования.
+
+const highestIncreasingSequence = array => {
+
 };
